fca55b6cbd5de859f209f2b93d4a65c9e3eb46fa
Rishabh Sharma <rish.sharma9012@gmail.com>
2021-09-01 10:39:30 +0530

    first commit

---
+++ src/main/java/com/cloud/config/SpringCloudConfigServerApplication.java
@@ -0,0 +1,19 @@
+package com.cloud.config;
+
+import java.util.ArrayList;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@SpringBootApplication
+@EnableConfigServer
+public class SpringCloudConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCloudConfigServerApplication.class, args);
+		
+		System.out.println("hello");
+	}
+
+}
