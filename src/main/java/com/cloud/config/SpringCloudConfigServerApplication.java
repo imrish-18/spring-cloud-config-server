dbe757cd79e4affd5c75397150a965545376ee4f
Rishabh Sharma <rish.sharma9012@gmail.com>
2021-09-06 15:04:43 +0530

    commit

--- src/main/java/com/cloud/config/SpringCloudConfigServerApplication.java
+++ src/main/java/com/cloud/config/SpringCloudConfigServerApplication.java
@@ -6,10 +6,19 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
+// TODO: Auto-generated Javadoc
+/**
+ * The Class SpringCloudConfigServerApplication.
+ */
 @SpringBootApplication
 @EnableConfigServer
 public class SpringCloudConfigServerApplication {
 
+	/**
+	 * The main method.
+	 *
+	 * @param args the arguments
+	 */
 	public static void main(String[] args) {
 		SpringApplication.run(SpringCloudConfigServerApplication.class, args);
 		
--- src/test/java/com/cloud/config/SpringCloudConfigServerApplicationTests.java
+++ src/test/java/com/cloud/config/SpringCloudConfigServerApplicationTests.java
@@ -3,9 +3,16 @@ package com.cloud.config;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+// TODO: Auto-generated Javadoc
+/**
+ * The Class SpringCloudConfigServerApplicationTests.
+ */
 @SpringBootTest
 class SpringCloudConfigServerApplicationTests {
 
+	/**
+	 * Context loads.
+	 */
 	@Test
 	void contextLoads() {
 	}
